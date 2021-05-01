3a8e0fbe5e6dcd0366a9aa7bfd6fdfd9024724ef
omerbesent <omer.besent@hotmail.com>
2021-05-01 04:23:30 +0300

    boş login ve register ekranı oluşturuldu

---
+++ src/screens/Login/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native'
+
+export class Register extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>Register</Text>
+                <TouchableOpacity>Logine dön</TouchableOpacity>
+            </SafeAreaView>
+        )
+    }
+}
+
+
---
+++ src/screens/Register/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native'
+
+export class Login extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>Login</Text>
+                <TouchableOpacity>Registera git</TouchableOpacity>
+            </SafeAreaView>
+        )
+    }
+}
+
+
